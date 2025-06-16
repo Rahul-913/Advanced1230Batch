23caf3eda6eb4641b8077c0ecb218060982afeb5
DELL <DELL@DESKTOP-A0FLEGI>
2025-06-16 13:06:57 +0530

    rekha

---
+++ Orange/src/test/java/Runnerscript/Rekha.java
@@ -0,0 +1,13 @@
+package Runnerscript;
+
+import org.testng.annotations.Test;
+
+public class Rekha
+{
+	@Test
+	public void tc1()
+	{
+		System.out.println("Hi Rahul sir ,I will miss selenium classes ");
+	}
+
+}
